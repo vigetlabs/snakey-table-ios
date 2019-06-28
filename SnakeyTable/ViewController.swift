f6335548d05cacc16b33aa7cca66e68905500e53
Prayash Thapa <prayasht@gmail.com>
2019-06-28 15:39:47 -0600

    Initial Commit

---
+++ SnakeyTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnakeyTable
+//
+//  Created by Prayash Thapa on 6/28/19.
+//  Copyright © 2019 Viget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
